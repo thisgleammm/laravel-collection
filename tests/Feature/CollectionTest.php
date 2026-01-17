6fad0fe00a5014a7a49a52fe64f184875f756d34
thisgleammm <thisgleam@gmail.com>
2026-01-17 17:19:06 +0700

    test: add CollectionTest feature test

---
+++ tests/Feature/CollectionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testCreateCollection(): void
+    {
+        $collection = collect([1, 2, 3]);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+    }
+}
