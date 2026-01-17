47bc617491340c77a6f4d564511caa3cac33368f
thisgleammm <thisgleam@gmail.com>
2026-01-17 18:16:15 +0700

    test: add slice test to CollectionTest

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -258,4 +258,20 @@ class CollectionTest extends TestCase
             ])
         ], $result->all());
     }
+
+    public function testSlice()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $result = $collection->slice(3);
+
+        $this->assertEqualsCanonicalizing([
+            4, 5, 6, 7, 8, 9
+        ], $result->all());
+
+        $result = $collection->slice(3, 2);
+
+        $this->assertEqualsCanonicalizing([
+            4, 5
+        ], $result->all());
+    }
 }
